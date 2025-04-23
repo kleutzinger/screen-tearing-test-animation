f74e4fe1381ab8da87a4ecf14b7838b698a2ea73
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2025-04-22 23:51:40 -0400

    drawing one line
    delete

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 from PIL import Image, ImageDraw
 
 HEIGHT = 1080
@@ -6,27 +7,37 @@ WIDTH = 1920
 WHITE = 255
 BLACK = 0
 
-MID_X = WIDTH//2
-MID_Y = HEIGHT//2
+MID_X = WIDTH // 2
+MID_Y = HEIGHT // 2
 
-def main():
-    im = Image.new('L', (WIDTH,HEIGHT), WHITE)
+CENTER = (MID_X, MID_Y)
+
+
+def rot_cross_image(radians: float, filename="out.png") -> None:
+    im = Image.new("L", (WIDTH, HEIGHT), WHITE)
     # modes: https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes
 
     draw = ImageDraw.Draw(im)
-    #draw.line((0, 0) + im.size, fill=128, width=100)
-    draw.line((MID_X, 0, MID_X, HEIGHT), fill=BLACK, width=50)
-    draw.line((0, MID_Y, WIDTH, MID_Y), fill=BLACK, width=50)
-    #draw.line((0, im.size[1], im.size[0], 0), fill=128)
+    # draw.line((0, 0) + im.size, fill=128, width=100)
+    r = WIDTH * 2
 
-    im.save('img.png')
-    import subprocess
-    subprocess.run(['feh', 'img.png'])
+    x = np.cos(radians) * r
+    x += MID_X
+    y = np.sin(radians) * r
+    y += MID_Y
+    draw.line((WIDTH - x, HEIGHT - y, x, y), fill=BLACK, width=50)
+
+    im.save(filename)
+
+
+def main():
+    i = 0
+    for p in np.linspace(0, np.pi, 60):
+        fname = f"img/{i:05}.png"
+        rot_cross_image(p, fname)
+        print(i)
+        i += 1
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
